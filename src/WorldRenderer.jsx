dce80b425a5a371e45f74105cf70d2437288142d
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-15 19:38:19 +0000

    Slowly rotate view until the user interacts with it

--- src/WorldRenderer.jsx
+++ src/WorldRenderer.jsx
@@ -5,12 +5,16 @@ import World from './World.jsx';
 
 export default function({ baseUrl = ``,
                           datasetFilePath = `/data/platynereis_dumerilii_32203/dataset.json`,
-                          informationLayerFilePath = `` }) {
+                          informationLayerFilePath = ``,
+                          mountNode = document.getElementById(`container`)}) {
 
     const datasetFileUrl = datasetFilePath ? Url.resolve(baseUrl, datasetFilePath) : ``;
     const informationLayerFileUrl = informationLayerFilePath ? Url.resolve(baseUrl, informationLayerFilePath) : ``;
 
-    ReactDOM.render(<World datasetFileUrl={datasetFileUrl} informationLayerFileUrl={informationLayerFileUrl}/>,
-        document.getElementById("container"));
+    ReactDOM.render(
+        <World datasetFileUrl={datasetFileUrl}
+               informationLayerFileUrl={informationLayerFileUrl}
+        />,
+        mountNode);
 
 };
