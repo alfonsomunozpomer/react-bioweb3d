7a30039f747012f99f1d112c22f3ef48e02efac4
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-15 15:59:58 +0000

    Allow to pass a base URL to build the dataset and information layer files target URLs; add an empty value to show raw data

--- src/WorldRenderer.jsx
+++ src/WorldRenderer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Url from 'url';
 import World from './World.jsx';
 
-export default function(datasetFileUrl = `/data/platynereis_dumerilii_32203/dataset.json`,
-                        informationLayerFileUrl = `/data/platynereis_dumerilii_32203/C5x79.1M.json`) {
+export default function({ baseUrl = ``,
+                          datasetFilePath = `/data/platynereis_dumerilii_32203/dataset.json`,
+                          informationLayerFilePath = `` }) {
+
+    const datasetFileUrl = Url.resolve(baseUrl, datasetFilePath),
+          informationLayerFileUrl = Url.resolve(baseUrl, informationLayerFilePath);
+
     ReactDOM.render(<World datasetFileUrl={datasetFileUrl} informationLayerFileUrl={informationLayerFileUrl}/>,
         document.getElementById("container"));
+
 };
