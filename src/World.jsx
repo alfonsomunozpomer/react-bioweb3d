1fdede9fd91589acf89060b09947f6eec3c3eb67
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-15 16:01:53 +0000

    Move all the Refetch logic to the parent component and avoid setting state in render using variables that are later injected in the return value as needed; use two components to render two sets of points: the raw data and the information layer overlaid on top

--- src/World.jsx
+++ src/World.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import React3 from 'react-three-renderer';
 import * as THREE from 'three';
 import TrackballControls from '../lib/trackball';
+import { connect, PromiseState } from 'react-refetch';
 
-import DataSet from './DataSet.jsx'
+import DataSet from './DataSet.jsx';
+import InformationLayer from './InformationLayer.jsx';
 
 class World extends React.Component {
     constructor(props, context) {
@@ -12,23 +14,52 @@ class World extends React.Component {
 
         this.state = {
             mainCameraPosition: new THREE.Vector3(100, 100, 100),
-            title: ``,
-            subHeader: ``
+            title: `Loading...`,
+            subHeader: `Please wait`
         };
     }
 
     render() {
         const width = this.props.width; // canvas width
         const height = this.props.height; // canvas height
+        let title = ``;
+        let subHeader = ``;
+
+        const {datasetFetch, informationLayerFetch} = this.props;
+
+        let dataset = null;
+        let informationLayer = null;
+        if (datasetFetch.pending) {
+            title = `Loading...`;
+        } else if (datasetFetch.rejected) {
+            title = `Error: the reference dataset couldn’t be loaded`;
+        } else if (datasetFetch.fulfilled) {
+            const datasetModel = datasetFetch.value.dataset;
+
+            title = datasetModel.name;
+            dataset = <DataSet points={datasetModel.points.map(this._computeCenterFunction(datasetModel.points))} />;
+
+            if (informationLayerFetch.fulfilled) {
+                const informationLayerModel = informationLayerFetch.value;
+
+                subHeader = informationLayerModel.name;
+                informationLayer =
+                    <InformationLayer
+                        points={datasetModel.points.map(this._computeCenterFunction(datasetModel.points))}
+                        numClass={informationLayerModel.numClass}
+                        values={informationLayerModel.values}
+                    />
+            }
+        }
 
         return (
             <div>
                 <div style={{position:`absolute`, color:`white`}}>
                     <div>
-                        <p style={{fontSize: `20px`, fontWeight: `bold`}}>{this.state.title}</p>
+                        <p style={{fontSize: `20px`, fontWeight: `bold`}}>{title}</p>
                     </div>
                     <div>
-                        <p>{this.state.subHeader}</p>
+                        <p>{subHeader}</p>
                     </div>
                 </div>
                 <React3 ref={react3 =>  { this.react3 = react3; }}
@@ -44,9 +75,8 @@ class World extends React.Component {
                                            near={1}
                                            far={10000}
                                            position={this.state.mainCameraPosition} />
-                        <DataSet datasetFileUrl={this.props.datasetFileUrl}
-                                 informationLayerFileUrl={this.props.informationLayerFileUrl}
-                                 setSceneCaptionsCallback={this._setTitleAndSubHeader} />
+                        {dataset}
+                        {informationLayer}
                     </scene>
                 </React3>
             </div>
@@ -87,11 +117,13 @@ class World extends React.Component {
         this.controls.update()
     }
 
-    _setTitleAndSubHeader = (title, subHeader) => {
-        this.setState({
-            title: title,
-            subHeader: subHeader
-        })
+    _computeCenterFunction = (points) => {
+        const means =
+            points.reduce(
+                (acc, cur) => [acc[0] + cur[0], acc[1] + cur[1], acc[2] + cur[2]], [0.0, 0.0, 0.0])
+                .map(m => m / points.length);
+
+        return (point) => [point[0] - means[0], point[1] - means[1], point[2] - means[2]];
     }
 }
 
@@ -99,7 +131,11 @@ World.propTypes = {
     width: React.PropTypes.number,
     height: React.PropTypes.number,
     datasetFileUrl: React.PropTypes.string,
-    informationLayerFileUrl: React.PropTypes.string
+    informationLayerFileUrl: React.PropTypes.string,
+    datasetFetch: React.PropTypes.instanceOf(PromiseState),
+    informationLayerFetch: React.PropTypes.instanceOf(PromiseState),
+    updateStatus: React.PropTypes.func,
+    updateStatusResponse: React.PropTypes.instanceOf(PromiseState) // will not be set until after `updateStatus()` is called
 };
 
 World.defaultProps = {
@@ -107,5 +143,7 @@ World.defaultProps = {
     height: 600
 };
 
-
-export default World;
+export default connect(props => ({
+    datasetFetch: props.datasetFileUrl,
+    informationLayerFetch: props.informationLayerFileUrl
+}))(World)
