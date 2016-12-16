272fa369058f00019eeb7d5e5b62c2692d3284ae
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-16 19:25:52 +0000

    The array clusteredPoints has now as many entries as necessary, ignores numClass because we don’t need it; support for colouring schemes: ranked and random/rainbow

--- src/InformationLayer.jsx
+++ src/InformationLayer.jsx
@@ -10,38 +10,49 @@ class InformationLayer extends React.Component {
             `Reference dataset has ${this.props.points.length.toLocaleString('en')} points and information ` +
             `layer has ${this.props.values.length.toLocaleString('en')} points. Probably this information layer is ` +
             `intended to be used with a different dataset! Rendering anyway...`);
+
     }
 
     render() {
         const clusteredPoints = [];
-        for (let i = 0; i < this.props.numClass; i++) {
-            clusteredPoints.push([]);
-        }
-        this.props.values.forEach((clusterIndex, pointIndex) => clusteredPoints[clusterIndex - 1].push(this.props.points[pointIndex]));
+        this.props.values.forEach((clusterIndex, pointIndex) => {
+            if (!clusteredPoints[clusterIndex - 1]) {
+                clusteredPoints[clusterIndex - 1] = [];
+            }
+            clusteredPoints[clusterIndex - 1].push(this.props.points[pointIndex])
+        });
 
-        const clusters = clusteredPoints.map((pointsCluster, clusterIndex) =>
+        this.clusters = clusteredPoints.map((pointsCluster, clusterIndex) =>
             <points key={`cluster_${clusterIndex}`}>
                 <geometry
                     vertices={pointsCluster.map(triplet => new THREE.Vector3(triplet[0], triplet[1], triplet[2]))}/>
                 <pointsMaterial color={this._colorGradient(clusterIndex)} size={this._pointSize(clusterIndex)}
                                 opacity={0.8} transparent={true}/>
-            </points>
-        );
+            </points>);
 
         return (
             <group>
-                {clusters}
+                {this.clusters}
             </group>
         );
     }
 
+    _random = (min, max) => {
+        const roundedMin = Math.ceil(min);
+        const roundedMax = Math.floor(max);
+        return Math.floor(Math.random() * (roundedMax - roundedMin + 1)) + roundedMin;
+    }
     _colorGradient = (v) => {
-        const firstRelevantClusters = 5;
-        if (v > firstRelevantClusters) {
-            return 0x0000ff;
+        if (this.props.colourScheme === `ranked`) {
+            const firstRelevantClusters = 5;
+            if (v > firstRelevantClusters) {
+                return 0x0000ff;
+            } else {
+                // Red with a speck of white
+                return Math.floor((255 << 16) + (v * 255 / firstRelevantClusters << 8) + (v * 255 / firstRelevantClusters));
+            }
         } else {
-            // Red with a speck of white
-            return Math.floor((255 << 16) + (v * 255 / firstRelevantClusters << 8) + (v * 255 / firstRelevantClusters));
+            return this._random(0x000000, 0xffffff);
         }
     }
 
@@ -59,7 +70,12 @@ class InformationLayer extends React.Component {
 InformationLayer.propTypes = {
     points: React.PropTypes.arrayOf(arrayOfThreeNumbers),
     numClass: React.PropTypes.number,
-    values: React.PropTypes.arrayOf(React.PropTypes.number)
+    values: React.PropTypes.arrayOf(React.PropTypes.number),
+    colourScheme: React.PropTypes.oneOf([`ranked`, `rainbow`])
+};
+
+InformationLayer.defaultProps = {
+    colourScheme: `ranked`
 };
 
 export default InformationLayer;
