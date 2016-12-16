660fb7fa954baac85e4f33700609b3bbf7639cd0
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-12-16 19:26:36 +0000

    Remove default parameters, they kind of get in the middle when testing

--- src/WorldRenderer.jsx
+++ src/WorldRenderer.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import Url from 'url';
 import World from './World.jsx';
 
-export default function({ baseUrl = ``,
-                          datasetFilePath = `/data/platynereis_dumerilii_32203/dataset.json`,
-                          informationLayerFilePath = ``,
-                          mountNode = document.getElementById(`container`)}) {
+export default function({ baseUrl,
+                          datasetFilePath,
+                          informationLayerFilePath,
+                          colourScheme,
+                          mountNode = document.getElementById(`container`),}) {
 
     const datasetFileUrl = datasetFilePath ? Url.resolve(baseUrl, datasetFilePath) : ``;
     const informationLayerFileUrl = informationLayerFilePath ? Url.resolve(baseUrl, informationLayerFilePath) : ``;
@@ -14,6 +15,7 @@ export default function({ baseUrl = ``,
     ReactDOM.render(
         <World datasetFileUrl={datasetFileUrl}
                informationLayerFileUrl={informationLayerFileUrl}
+               colourScheme={colourScheme}
         />,
         mountNode);
 
